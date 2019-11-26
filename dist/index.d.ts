d158e450a61fa05757a3af3d1eba17545f99f470
Chris Weaver <cmagweaver@gmail.com>
2019-11-26 10:24:51 -0800

    feat: make application protocols into a package (#10)
    * feat: add validation for LTE messages
    
    - Add entry point in dist/index.js
    - index.ts takes a message and validates appId, messageType, and data
    
    * build: change tsconfig to target es2015
    
    * build: include dist

---
+++ dist/index.d.ts
@@ -0,0 +1,12 @@
+export declare enum AppId {
+    Gps = "GPS",
+    Flip = "FLIP",
+    Gen = "GEN",
+    Temp = "TEMP",
+    Humid = "HUMID",
+    AirPress = "AIR_PRESS",
+    RSRP = "RSRP",
+    Button = "BUTTON",
+    Device = "DEVICE"
+}
+export declare const isMessageValid: (message: any) => boolean;
