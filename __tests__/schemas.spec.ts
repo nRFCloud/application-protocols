d1d261447f19d4f20ea9746903f71891feafd1a6
Chris Weaver <cmagweaver@gmail.com>
2020-12-10 16:46:05 -0800

    feat: add config for air quality

--- __tests__/schemas.spec.ts
+++ __tests__/schemas.spec.ts
@@ -14,7 +14,10 @@ schemas.forEach(schema => {
     describe(app, () => {
         const s = JSON.parse(fs.readFileSync(schema, 'utf-8'));
         const examples = globSync(
-            path.join(path.resolve(__dirname, '..', 'examples', app), '*.json'),
+            path.join(
+                path.resolve(__dirname, '..', 'examples', app, '**'),
+                '*.json',
+            ),
         );
         examples.forEach(example => {
             const data = JSON.parse(fs.readFileSync(example, 'utf-8'));
