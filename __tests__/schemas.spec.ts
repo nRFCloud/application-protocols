1e49d05e92ee2f80cf9689822af7f2c4148107b4
Chris Weaver <cmagweaver@gmail.com>
2021-03-01 21:42:02 -0800

    feat: update tests

--- __tests__/schemas.spec.ts
+++ __tests__/schemas.spec.ts
@@ -5,17 +5,53 @@ const fs = require('fs');
 const path = require('path');
 import { sync as globSync } from 'glob';
 
-const schemas = globSync(
-    path.join(path.resolve(__dirname, '..', 'schemas'), '*.json'),
+const cloudToDeviceSchemas = globSync(
+    path.join(
+        path.resolve(__dirname, '..', 'schemas', 'cloudToDevice'),
+        '*.json',
+    ),
 );
 
-schemas.forEach(schema => {
+cloudToDeviceSchemas.forEach(schema => {
     const app = path.parse(schema).name;
     describe(app, () => {
         const s = JSON.parse(fs.readFileSync(schema, 'utf-8'));
         const examples = globSync(
             path.join(
-                path.resolve(__dirname, '..', 'examples', app, '**'),
+                path.resolve(__dirname, '..', 'examples', 'cloudToDevice', app),
+                '*.json',
+            ),
+        );
+        examples.forEach(example => {
+            const data = JSON.parse(fs.readFileSync(example, 'utf-8'));
+            test(`${data.appId}: ${data.messageType} ${
+                data.data ? JSON.stringify(data.data) : ''
+            }`, () => {
+                const validate = ajv.compile(s);
+                const valid = validate(data);
+                if (!valid) {
+                    console.error(validate.errors);
+                }
+                expect(valid).toEqual(true);
+            });
+        });
+    });
+});
+
+const deviceToCloudSchemas = globSync(
+    path.join(
+        path.resolve(__dirname, '..', 'schemas', 'deviceToCloud'),
+        '*.json',
+    ),
+);
+
+deviceToCloudSchemas.forEach(schema => {
+    const app = path.parse(schema).name;
+    describe(app, () => {
+        const s = JSON.parse(fs.readFileSync(schema, 'utf-8'));
+        const examples = globSync(
+            path.join(
+                path.resolve(__dirname, '..', 'examples', 'deviceToCloud', app),
                 '*.json',
             ),
         );
