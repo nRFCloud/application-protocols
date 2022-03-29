a2dd734a0ada5747ed2204a86edf7a31f64c2d59
Cole Hafner <colehafner@gmail.com>
2022-03-29 12:01:29 -0700

    feat: add support for ground fix (#38)
    * feat: added support for ground_fix
    
    * chore: added pr template
    
    * fix: fix typo
    
    * refactor: add template to .github dir
    
    * fix: fixed filenames

--- __tests__/schemas.spec.ts
+++ __tests__/schemas.spec.ts
@@ -11,40 +11,44 @@ import { isValidSchema, getValidationWithDependencies } from '../utilities';
 
 // @ts-ignore
 const testGroup = ({ schemaName, schema, schemaTests }) => {
-    const chosen = Array.isArray(schemaTests) ? [schemaTests] : schemaTests;
+    // pgps has an array response that is valid that needs to be converted to an array of arrays
+    const chosen =
+        schemaName === 'pgps' && Array.isArray(schemaTests)
+            ? [schemaTests]
+            : schemaTests;
+
     test.each<Schema>(chosen)('%p', (example) => {
         expect(isValidSchema(schema, example)).toEqual(true);
     });
 };
 
-describe('Validate examples for cloud to device', () => {
-    const { schemasRecords } = getSchemaTestCollection(
-        SchemaCollectionName.CloudToDevice,
-    );
-    describe.each<SchemaRecord>(schemasRecords)(
-        '$schemaName',
-        testGroup
-    );
-});
+const header = (name: string) => `
+***************************************************
+${name.toUpperCase()}
+***************************************************
+`;
 
-describe('Validate examples for device to cloud', () => {
+describe(header('device >>> cloud'), () => {
     const { schemasRecords } = getSchemaTestCollection(
         SchemaCollectionName.DeviceToCloud,
     );
-    describe.each<SchemaRecord>(schemasRecords)(
-        '$schemaName',
-        testGroup,
-    );
+
+    // console.log(`schemaRecords, ${JSON.stringify(schemasRecords, null, 2)}`);
+    describe.each<SchemaRecord>(schemasRecords)('$schemaName', testGroup);
 });
 
-describe('Validate examples for the device shadow', () => {
+describe(header('cloud >>> device'), () => {
+    const { schemasRecords } = getSchemaTestCollection(
+        SchemaCollectionName.CloudToDevice,
+    );
+    describe.each<SchemaRecord>(schemasRecords)('$schemaName', testGroup);
+});
+
+describe(header('device shadow'), () => {
     const { schemasRecords } = getSchemaTestCollection(
         SchemaCollectionName.DeviceShadow,
     );
-    describe.each<SchemaRecord>(schemasRecords)(
-        '$schemaName',
-        testGroup,
-    );
+    describe.each<SchemaRecord>(schemasRecords)('$schemaName', testGroup);
 });
 
 describe('Validate example for dependencies', () => {
