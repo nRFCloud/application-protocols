264c623ee163653ce4cfdf22f3a02f9fc84c68b7
Stephen McGuckin <stephen.mcguckin@nordicsemi.no>
2023-01-19 10:42:45 -0800

    some code cleanup

--- utilities.ts
+++ utilities.ts
@@ -1,6 +1,6 @@
 import Ajv, { Schema } from 'ajv/dist/2020';
 
-export const isValidSchema = (schema: Schema, example: Schema) => {
+export const isValidSchema = (schema: Schema, example: unknown): boolean => {
     const ajv = new Ajv({
         strict: "log",
         verbose: true,
@@ -15,7 +15,7 @@ export const isValidSchema = (schema: Schema, example: Schema) => {
     return result
 };
 
-export const getValidationWithDependencies = (schema: Schema, dependencies: Schema[], example: Schema) => {
+export const getValidationWithDependencies = (schema: Schema, dependencies: Schema[], example: unknown): boolean => {
     // When dealing with multiple schemas, you need to make sure they are all loaded before trying to validate
     // Currently, DeviceShadow depends on Config being loaded, so you would need to call addSchema with Config
     const ajv = new Ajv();
