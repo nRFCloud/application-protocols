4070438da23ed8b55d799faa60047ad6e4ef2ea8
Cole Hafner <colehafner@gmail.com>
2023-10-17 12:46:13 -0700

    refactor: removed 'pgnss' bit from tests

--- __tests__/schemas.spec.ts
+++ __tests__/schemas.spec.ts
@@ -11,9 +11,9 @@ import { isValidSchema, getValidationWithDependencies } from '../utilities';
 
 // @ts-ignore
 const testGroup = ({ schemaName, schema, schemaTests }) => {
-    // pgps and pgnss have an array response that is valid that needs to be converted to an array of arrays
+    // pgps has an array response that is valid that needs to be converted to an array of arrays
     const chosen =
-        ['pgps', 'pgnss'].includes(schemaName.toLowerCase()) && Array.isArray(schemaTests)
+        schemaName === 'pgps' && Array.isArray(schemaTests)
             ? [schemaTests]
             : schemaTests;
 
